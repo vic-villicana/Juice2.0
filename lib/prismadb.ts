2c6dc0d3ab018ed1a80884f4224cd5e2bbaebc08
Victor Uribe <50690167+vic-villicana@users.noreply.github.com>
2024-03-26 15:13:41 -0700

    install and initiate prisma client and connect to mongodb cluster

---
+++ lib/prismadb.ts
@@ -0,0 +1,13 @@
+import {PrismaClient} from '@prisma/client'
+
+declare global {
+    var prisma: PrismaClient | undefined
+}
+
+const client = globalThis.prisma || new PrismaClient()
+
+if(process.env.Node_env !== 'production'){
+    globalThis.prisma = client
+}
+
+export default client
---
+++ pages/api/items/index.ts
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from '@/lib/prismadb'
+
+export default async function handler(req:NextApiRequest, res:NextApiResponse) {
+
+    if(req.method === 'GET'){
+        try{
+            const items = await prisma.item.findMany({})
+            console.log('GET_ITEMS', items)
+            return res.json(items)
+    
+        }catch(err){
+            console.log('ITEMS_GET', err)
+            // return res.status(500).json({
+            //     status:'internal server error',
+            // })
+        }
+    }
+    
+}
+
+
