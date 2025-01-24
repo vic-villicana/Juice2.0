0e9ba3c9b7f47c2c7c730fbb406bf8cc021d9aa1
Victor Uribe <50690167+vic-villicana@users.noreply.github.com>
2025-01-24 12:05:51 -0800

    disable socket server

--- pages/socket.tsx
+++ pages/socket.tsx
@@ -1,6 +1,6 @@
-import io from 'socket.io-client'
+// import io from 'socket.io-client'
 
-const socket = io("http://localhost:8000")
+// const socket = io("http://localhost:8000")
 
 
-export default socket
+// export default socket
